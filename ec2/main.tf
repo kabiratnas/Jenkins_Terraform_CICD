22e69bf7226ba8b5d94cb81460b9de24627aa10c
kabiratnas <153971340+kabiratnas@users.noreply.github.com>
2025-03-23 17:42:59 -0500

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,33 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Create VPC
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr
+  cidr_block = "172.31.0.0/16"
   instance_tenancy = "default"
-
   tags = {
     Name = "main"
   }
 }
 
-# Create Subnet in VPC
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone        = "us-east-1a"
 
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
-# Create Security Group with firewall rules
+#Create security group with firewall rules
 resource "aws_security_group" "new-terraform-sg" {
   name        = var.security_group
-  description = "Security group for Jenkins"
-
+  description = "security group for jenkins"
+                                        
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -36,15 +23,14 @@ resource "aws_security_group" "new-terraform-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Restrict RDP access to a specific IP (replace YOUR_IP)
-  ingress {
+ ingress {
     from_port   = 3389
     to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = var.allowed_ips
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
+ # outbound from Jenkins server
   egress {
     from_port   = 0
     to_port     = 65535
@@ -52,19 +38,18 @@ resource "aws_security_group" "new-terraform-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags= {
     Name = var.security_group
   }
 }
 
-# Create EC2 Instance
 resource "aws_instance" "terraform-ec2" {
-  count                  = var.instance_count
-  ami                    = var.ami_id
-  key_name               = var.key_name
-  instance_type          = var.instance_type
+  count = var.instance_count
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.new-terraform-sg.id]
-  tags = {
+  tags= {
     Name = var.tag_name
   }
 }
