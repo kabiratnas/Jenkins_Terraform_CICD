844d5074f715efad13d42a1d900e2f50d721433d
kabiratnas <153971340+kabiratnas@users.noreply.github.com>
2025-03-23 16:02:41 -0500

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Mytfkey"
+    default     =  "jenkins-key"
 }
 
 variable "instance_type" { 
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "Ikechukwu-ec2" 
+    default     = "Server_dev" 
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
